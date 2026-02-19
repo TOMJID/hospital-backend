3086b1966aaccc73058a57603d49952ca0e0f8c2
TOMJID <tomjidhuda04@gmail.com>
2026-02-19 14:27:57 +0600

    feat: implement zod error handling and doctor update validation
    - Enhance global error handler to capture and format Zod validation errors into structured response
    - Add environment-based visibility for error stacks and details
    - Create Zod schema for partial doctor profile updates
    - Refactor doctor interface and update route to include request validation and support optional fields

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,32 +1,53 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from "express";
 import { envVars } from "../config/env";
 import status from "http-status";
+import z from "zod";
 
 export const globalErrorHandler = (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   err: any,
   req: Request,
   res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction,
 ) => {
   if (envVars.NODE_ENV === "development") {
     console.log(err);
   }
 
-  // eslint-disable-next-line prefer-const
   let statusCode: number = status.INTERNAL_SERVER_ERROR;
   let message: string = "Something went wrong";
   let error: string = "Internal Server Error";
+  const errorSource: { path: string; message: string; code: string }[] = [];
 
-  if (err instanceof Error) {
+  if (err instanceof z.ZodError) {
+    statusCode = status.BAD_REQUEST;
+    message = "Zod Validation error";
+
+    err.issues.forEach((issue) => {
+      errorSource.push({
+        path:
+          issue.path.length > 1
+            ? issue.path.join(" -> ")
+            : issue.path[0].toString(),
+        message: issue.message,
+        code: issue.code,
+      });
+    });
+
+    error = "Validation Error";
+  } else if (err instanceof Error) {
     message = err.message;
-    error = err.stack || "Internal Server Error";
+    error =
+      envVars.NODE_ENV === "development"
+        ? err.stack || "Internal Server Error"
+        : "Internal Server Error";
   }
 
   res.status(statusCode).json({
     success: false,
     message,
-    error,
+    error: envVars.NODE_ENV === "development" ? error : "Internal Server Error",
+    errorSource,
   });
 };
